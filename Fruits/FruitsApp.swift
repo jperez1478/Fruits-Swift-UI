0745236108683ee556497372a605ec1ef02a1f8d
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-06 01:33:32 -0500

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Jessica Perez on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
