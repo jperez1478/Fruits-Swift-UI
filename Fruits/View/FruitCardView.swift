c7792b31e728f2b30e2f7a74709c1f6e910629a4
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-06 23:18:16 -0500

    1- I added the image  in a vstack inside a zstack 2- the stack has a spacking of 20 to have the image in the middle 3- outside my stack i added the background color which works with linear gradients start point and endpoint with corner radios of 20

---
+++ Fruits/View/FruitCardView.swift
@@ -0,0 +1,45 @@
+//
+//  FruitCardView.swift
+//  Fruits
+//
+//  Created by Jessica Perez on 8/6/21.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    //MARK: - Properties
+    
+    
+    //MARK:- Body
+    var body: some View {
+        ZStack {
+            VStack(spacing: 20) {
+                //Fruit: Image
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x:6, y:8)
+                
+                
+                
+                //Fruit: Tittle
+                Text("Bluebbery")
+                //Fruit: Headline
+                //Button: Start
+            }//: end of vstack
+        }//: end of zstack
+        .background(LinearGradient(gradient: Gradient(colors:
+                                    [Color("ColorBlueberryLightr"),
+                                    Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+                                    .cornerRadius(20)
+    }
+}
+
+//MARK: - Preview
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
